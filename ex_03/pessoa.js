be1438a3e14d857422b8206cece9ee70dbfb21b1
guaracyalima <guaracyaraujolima@gmail.com>
2017-11-18 23:26:22 -0200

    exercice_03 and configs to transpiler to es6

---
+++ ex_03/pessoa.js
@@ -0,0 +1,14 @@
+//using class on javascript ES6
+//pra exportar (deixar disponivel pra uso externo)
+//deve-se exportar a classe pessoa
+export default class Pessoa {
+
+  constructor(nome){
+    this.nome = nome
+  }
+
+  toString(){
+    return `Pessoa: ${this.nome}`
+  }
+
+}

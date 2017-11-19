855eed7519c8c87057bb89aaadbdf5605507a8ec
guaracyalima <guaracyaraujolima@gmail.com>
2017-11-18 23:35:49 -0200

    pressets do babel spread

--- ex/index.js
+++ ex/index.js
@@ -1,5 +1,18 @@
-console.log('disgrama')
-import Pessoa from './pessoa'
+const produto = {
+  nome: 'Caneta bic preta',
+  preco: 1.90,
+  desconto: 0.05
+}
 
-const pessoa = new Pessoa('J Hetfield')
-console.log(pessoa.toString())
+
+function clone(objeto){
+  //operador spread serve pra clonar objetos
+  //sua instanciacao e com os 3 pontos (...)
+  return { ...objeto}
+}
+
+const novoProduto = clone(produto)
+
+novoProduto.nome = 'Buceta loirinha e cheirosa'
+
+console.log('shwoing new product ', novoProduto)
---
+++ ex_04/index.js
@@ -0,0 +1,5 @@
+console.log('disgrama')
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('J Hetfield')
+console.log(pessoa.toString())
